63c2a7f0c48bcd44a660ccb25a17a483aba1f2dc
jmontanholi <jonunes9326@hotmail.com.br>
2021-08-23 13:41:04 -0300

    Finish changes requested

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Books from './components/books';
+import Categories from './components/categories';
+import NavBar from './components/navBar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <NavBar />
+    <Switch>
+      <Route path="/categories">
+        <Categories />
+      </Route>
+      <Route path="/">
+        <Books />
+      </Route>
+    </Switch>
+  </Router>,
+
+  document.getElementById('root'),
+);
