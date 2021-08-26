416536d1a8789e81731fc82212e8469a818590e8
jmontanholi <jonunes9326@hotmail.com.br>
2021-08-26 12:12:59 -0300

    Finish nav styling

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,22 @@
 import { Link } from 'react-router-dom';
+import { BsFillPersonFill } from 'react-icons/bs';
+import style from './navBar.module.css';
 
 const NavBar = () => (
-  <div>
-    <Link to="/">Books</Link>
-    <Link to="/categories">Categories</Link>
-  </div>
+  <nav className={style.nav}>
+    <div className={style.div}>
+      <div className={style.insideDiv1}>
+        <h1 className={`${style.title} ${style.font1Bold}`}>Bookstore CMS</h1>
+        <div className={style.linksDiv}>
+          <Link to="/" className={`${style.link} ${style.font1Reg}`}>BOOKS</Link>
+          <Link to="/categories" className={style.link}>CATEGORIES</Link>
+        </div>
+      </div>
+      <div className={style.insideDiv2}>
+        <BsFillPersonFill className={style.icon} />
+      </div>
+    </div>
+  </nav>
 );
 
 export default NavBar;
